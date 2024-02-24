fd3d7909f8c514daec7372084a447fbb37c2a990
MbusyaNgesa <theengesa@gmail.com>
2024-02-24 18:41:59 +0300

    Cart Fucntionality

--- Components/CartItems/CartItems.jsx
+++ Components/CartItems/CartItems.jsx
@@ -5,11 +5,11 @@ import remove_icon from '../Assests/Assets/cart_cross_icon.png'
 
 
 const CartItems = () => {
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext);
 
   return (
     <div className='cartitems'>
-      <div className="cartitems-format-main">
+      <div className="cartitems-format-main ">
       <p>Products</p>
       <p>Title</p>
       <p>Price</p>
@@ -24,13 +24,13 @@ const CartItems = () => {
             if(cartItems[e.id]>0)
             {
                 return <div>
-                <div className="cartitems-format">
+                <div className="cartitems-format cartitems-format-main">
                     <img src={e.image} alt="" className='carticon-product-icon'  />
                     <p>{e.name}</p>
-                    <p>${e.new_price}</p>
+                    <p>KSh {e.new_price}</p>
                     <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                    <p>{e.new_price*cartItems[e.id]}</p>
-                    <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                    <p>KSh {e.new_price*cartItems[e.id]}</p>
+                    <img className='cartiitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
                 </div>
                 <hr />
                 
@@ -38,7 +38,36 @@ const CartItems = () => {
             }
             return null;
         })}
-        
+
+       <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1> Cart Total</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>KSh {getTotalCartAmount()} </p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>KSh {getTotalCartAmount()} </h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitem-promobox">
+            <input type="text" placeholder='promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+        </div>  
     </div>
   )
 }
