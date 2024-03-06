584102a70a6fffde00ec857f3edf927b68ce23c9
MbusyaNgesa <theengesa@gmail.com>
2024-03-06 17:30:09 +0300

    Sidebar

---
+++ src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ListProduct.css'
+
+const ListProduct = () => {
+  return (
+    <div classname='listproduct'>
+      
+    </div>
+  )
+}
+
+export default ListProduct
