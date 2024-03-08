c1d5fbe2cff3bf4dff56abb9bab8f7b210e0a09f
MbusyaNgesa <theengesa@gmail.com>
2024-03-08 23:31:39 +0300

    Toggle Hero

--- Components/Breadcrums/Breadcrum.jsx
+++ Components/Breadcrums/Breadcrum.jsx
@@ -4,6 +4,11 @@ import arrow_icon from '../Assests/Assets/breadcrum_arrow.png'
 
 const Breadcrum = (props) => {
     const {product} = props;
+
+    if (!product || !product.category || !product.name) {
+      return null; // Return null if product data is not available
+  }
+
   return (
     <div className='breadcrum'>
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
--- Components/Hero/Hero.jsx
+++ Components/Hero/Hero.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Hero.css'
 import hand_icon from '../Assests/Assets/hand_icon.png'
 import arrow_icon from '../Assests/Assets/arrow.png'
 import hero_image from '../Assests/Assets/hero_image.png'
-
+import Toggle from '../Toggle/Toggle'
+import useLocalStorage from 'use-local-storage'
 
 const Hero = () => {
+
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <div className='hero'>
+    <div className='hero' data-theme = { isDark ? "dark" : "light" } >
+
+      <Toggle 
+        isChecked={isDark}
+        handleChange={() => setIsDark(!isDark)}
+      />
+
       <div className="hero-left">
       <h2> Popular Rentals </h2>
         <div>
